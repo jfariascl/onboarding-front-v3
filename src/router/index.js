5874c1c0b582327f4e8c249bd8610c49655e8a54
Jorge Farías <jorge.jffs@gmail.com>
2024-05-10 09:20:06 -0400

    creación de rutas  y stores de sesiones

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { createWebHistory, createRouter } from "vue-router";
+
+const routes = [
+    {
+        path: "/",
+        name: "home",
+        component: Home,
+    },]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
